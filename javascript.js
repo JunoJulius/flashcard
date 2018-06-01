ab3c7b9efc5eff1cfa68196c379696c1fec8d4be
rbhgn <robhgn@gmail.com>
2018-06-01 11:58:09 +0200

    first version

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 
 
-let cards = []
-
+let cards = [];
+let activeCards =[];
 
 class Card {
   constructor(q, a) {
@@ -17,13 +17,38 @@ function addCard() {
   const answer    = document.getElementById("answer").value;
   const card      = new Card(question, answer);
   cards.push(card);
+  getIndexOfCard();
 }
 
 function getCard() {
-  let randomIndex = Math.floor(Math.random() * cards.length);
-  return cards[randomIndex];
+  if (activeCards.length > 0) {
+    let randomIndex = Math.floor(Math.random() * activeCards.length);
+    console.log(`Random ${randomIndex}`);
+    let tempIndex   = activeCards[randomIndex];
+    return cards[tempIndex];
+  } else {
+    console.log("You're Finished");
+  }
+  // let randomIndex = Math.floor(Math.random() * cards.length);
+  // if (cards[randomIndex].y) {
+  //   getCard();
+  // } else if (!cards[randomIndex].y) {
+  //   return cards[randomIndex];
+  //   console.log(getCard)
+  // } else {
+  //   console.log("DONE!");
+  // }
 }
 
+function addIndexToActiveCards(i) {
+    activeCards.push(i);
+}
+
+function getIndexOfCard() {
+  let index = cards.length - 1;
+  activeCards.push(index);
+  console.log(index);
+}
 
 function makeTestCards() {
   for (let i = 0; i < 12; i++) {
@@ -31,6 +56,7 @@ function makeTestCards() {
     const answer    = `Answer ${i}`;
     const card      = new Card(question, answer);
     cards.push(card);
+    getIndexOfCard();
   }
 }
 
