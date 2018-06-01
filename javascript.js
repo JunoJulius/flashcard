337447c7e8f599e0c55c0b661f951157be90cefd
rbhgn <robhgn@gmail.com>
2018-06-01 13:29:28 +0200

    test version of showCards

--- javascript.js
+++ javascript.js
@@ -23,32 +23,99 @@ function addCard() {
 function getCard() {
   if (activeCards.length > 0) {
     let randomIndex = Math.floor(Math.random() * activeCards.length);
-    console.log(`Random ${randomIndex}`);
     let tempIndex   = activeCards[randomIndex];
     return cards[tempIndex];
   } else {
     console.log("You're Finished");
   }
-  // let randomIndex = Math.floor(Math.random() * cards.length);
-  // if (cards[randomIndex].y) {
-  //   getCard();
-  // } else if (!cards[randomIndex].y) {
-  //   return cards[randomIndex];
-  //   console.log(getCard)
-  // } else {
-  //   console.log("DONE!");
-  // }
-}
-
-function addIndexToActiveCards(i) {
-    activeCards.push(i);
 }
 
 function getIndexOfCard() {
   let index = cards.length - 1;
   activeCards.push(index);
-  console.log(index);
 }
+
+function showCards() {
+  let lengte;
+  if (activeCards.length > 3) {
+    lengte = 4;
+  } else {
+    lengte = activeCards.length;
+  }
+ for (let i = 0 ;i < lengte; i++) {
+   let card = getCard();
+   let question = card.q;
+   let answer   = card.a;
+   let tries    = card.n;
+   console.log(question, answer, tries);
+
+
+
+  /// build DIV'S
+  const container = document.getElementById("flashcards");
+
+  const divFlashcard    = document.createElement("div");
+    divFlashcard.setAttribute('id',"flashcard");
+  const divFlashcardq   = document.createElement("div");
+    divFlashcardq.setAttribute('id',"flashcardq");
+  const divFlashcarda   = document.createElement("div");
+      divFlashcarda.setAttribute('id',"flashcarda");
+
+/// ADD text
+
+  const qtext   = document.createElement("p");
+  const q       = document.createTextNode(question);
+  qtext.appendChild(q);
+
+  const atext   = document.createElement("p");
+  const a       = document.createTextNode(question);
+  atext.appendChild(a);
+
+  // ADD BUTTONS
+
+  const answerS = document.createElement("input");
+    answerY.setAttribute('type', "submit");
+    answerY.setAttribute('id', "answerS");
+    answerY.setAttribute('onclick', "showAnwser()");
+
+  const answerY = document.createElement("input");
+    answerY.setAttribute('type', "submit");
+    answerY.setAttribute('id', "answery");
+    answerY.setAttribute('onclick', "buttonYes()");
+
+  const answerN = document.createElement("input");
+    answerY.setAttribute('type', "submit");
+    answerY.setAttribute('id', "answern");
+    answerY.setAttribute('onclick', "buttonNo()");
+
+/// Build Everything
+    divFlashcardq.appendChild(qtext);
+    divFlashcardq.appendChild(answerS);
+
+    divFlashcarda.appendChild(atext);
+    divFlashcarda.appendChild(answerY);
+    divFlashcarda.appendChild(answerN);
+
+    divFlashcard.appendChild(divFlashcardq);
+    divFlashcard.appendChild(divFlashcarda);
+    container.appendChild(divFlashcard);
+  }
+}
+
+function buttonYes() {
+  console.log("YES");
+
+}
+
+function buttonNo() {
+  console.log("NO");
+}
+
+function showAnwser() {
+
+}
+
+////////////////////////////////////
 
 function makeTestCards() {
   for (let i = 0; i < 12; i++) {
